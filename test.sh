3926df6bac183f435c0747d79438e6a4c4e0ec71
Mouad <mowdl@Mouads-MacBook-Pro.local>
2023-11-10 19:37:24 +0100

    rerun if failed some test

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 
 norminette libft/*.c libft/libft.h
 echo "--- Compiling libft"
-(cd libft && make) &&
+(cd libft && make && make bonus) &&
 	echo "--- Running tests" &&
-	(cd tests && cmake --build build && cd build && ctest)
+	(cd tests && cmake --build build && cd build && ctest || ctest --rerun-failed --output-on-failure)
